f9acd1cf0de8f21a6e51c014c554b179dee8553d
Ostrich96 <liiichen96@163.com>
2022-05-27 09:57:04 -0400

    Update myVector.cpp

--- myVector.cpp
+++ myVector.cpp
@@ -41,8 +41,70 @@ class myVector{
             rhs.objects = nullptr;
             rhs.theCapacity = 0;
         }
+        // increase capacity
+        void reserve(int newCapacity){
+            //if newCapacity is smaller than current size, return;
+            if(newCapacity < theSize){
+                return;
+            }
+            Object *newArray = new Object[newCapacity];//creat new array
+            for(int i = 0; i < theSize; i++){
+                newArray[k] = std::move(objects[k]); //move rvalue
+            }
+            theCapacity = newCapacity;
+            std::swap(objects,newArray);// swap the pointer
+            delete[]newArray;
+        }
+        void resize(int newSize){
+            if(newSize > theCapacity){
+                reserve(newSize*2)//keep the capacity twice as much as the size
+            }
+            theSize = newSize;
+        }
+        Object &operator [](int index){
+            return objects[index];
+        }
+        const Object & operator[](int index)const{// first const: return value is const; second const: not able to change objects;
+            return objects[index];
+        }
 
+        bool empty()const{
+            return theSize == 0;
+        }
+        void push_back(const Object & x){
+            if(theSize == theCapacity){
+                reserve(theCapacity*2+1);
+            }
+            objects[theSize++] = x;
+        }
+        void push_back(Object && x){
+            if(theSize == theCapacity){
+                reserve(theCapacity*2+1);
+            }
+            objects[theSize++] = std::move(x);
+        }
+        void pop_back(){
+            --theSize;
+        }
+        const Object & back()const{
+            return objects[theSize-1];
+        }
+        typedef Object* iterator;
+        typedef const Object* const_iterator;
 
+        iterator begin(){
+            return &objects[0];
+        }
+        const_iterator begin()const{
+            return &objects[0];
+        }
+        iterator end(){
+            return &objects[theSize];
+        }
+        const_iterator end()const {
+            return &objects[theSize];
+        }
+        static const int SPARE_CAPACITY = 16;
 
     private:
         int theSize;
